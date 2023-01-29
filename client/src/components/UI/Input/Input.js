348916a45b36ad72a7be3c0904ec01df85811dbf
SohamGanmote <sohamganmote@gmail.com>
2023-01-29 12:42:15 +0530

    new Signup page logic

--- client/src/components/UI/Input/Input.js
+++ client/src/components/UI/Input/Input.js
@@ -5,7 +5,14 @@ const Input = (props) => {
   return (
     <div className={styles["input-group"]}>
       <label htmlFor={props.id}>{props.label}</label>
-      <input type={props.type} id={props.id} placeholder={props.placeholder} />
+      <input
+        type={props.type}
+        id={props.id}
+        placeholder={props.placeholder}
+        onChange={props.onChange}
+        value={props.value}
+        ref={props.ref}
+      />
     </div>
   );
 };
