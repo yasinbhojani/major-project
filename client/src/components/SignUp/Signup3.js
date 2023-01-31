5266fe26a7072cd9a48fd71e7f57329fe87caa1e
SohamGanmote <sohamganmote@gmail.com>
2023-01-31 21:21:45 +0530

    OTP Button Added

--- client/src/components/SignUp/Signup3.js
+++ client/src/components/SignUp/Signup3.js
@@ -1,9 +1,16 @@
 import Input from "../UI/Input/Input";
 import btnstyles from "../../css/button.module.css";
-
+import styles from "../Login/Login.module.css";
+import { useState } from "react";
 const Signup3 = (props) => {
+  const [optBTN, setOtpBTN] = useState(true);
   const optChangeHandler = (event) => {
     props.setOtp(event.target.value);
+  };
+  setTimeout(() => setOtpBTN(false), 5000);
+  const resendOTPHandler = () => {
+    setOtpBTN(true);
+    setTimeout(() => setOtpBTN(false), 5000);
   };
   return (
     <>
@@ -13,9 +20,16 @@ const Signup3 = (props) => {
           type="password"
           label="Enter OTP"
           placeholder="Enter OTP Sent To Your Mail"
-          onchange={optChangeHandler}
+          onChange={optChangeHandler}
         />
-        <button type="submit" className={`${btnstyles.btn} ${btnstyles.login}`}>
+        <button
+          disabled={optBTN}
+          onClick={resendOTPHandler}
+          className={styles.btnLink}
+        >
+          Resend OTP
+        </button>
+        <button className={`${btnstyles.btn} ${btnstyles.login}`}>
           VERIFY
         </button>
       </form>
