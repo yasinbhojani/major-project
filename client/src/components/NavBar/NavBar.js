b66e4ccf9c3a5e25679e356f9f3dd17d2b60fc40
SohamGanmote <sohamganmote@gmail.com>
2023-02-06 20:48:46 +0530

    Nav Bar Fixed

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,19 +1,26 @@
 import styles from "./NavBar.module.css";
 import logo from "../../assets/shell-logo.png";
 import NavButtons from "./NavButtons";
+
+import home from "./icon/home.png";
+import news from "./icon/news.png";
+import chats from "./icon/chat.png";
+import profile from "./icon/profile.png";
+
 const NavBar = (props) => {
   return (
     <>
-      <nav className={styles.NavBarBody}>
-        <div className={styles.NavBar}>
-          <a href="/">
-            <button className={styles.NavBarLogo}>
-              <img src={logo} alt="logo" />
-            </button>
-          </a>
-          <NavButtons />
-          <button className={styles.newPearl}>New Pearl</button>
-        </div>
+      <nav className={styles.NavBar}>
+        <a href="/">
+          <button className={styles.NavBarLogo}>
+            <img src={logo} alt="logo" />
+          </button>
+        </a>
+        <NavButtons path="/" iconSource={home} page="home" />
+        <NavButtons path="/news" iconSource={news} page="News" />
+        <NavButtons path="/chats" iconSource={chats} page="Chats" />
+        <NavButtons path="/profile" iconSource={profile} page="Profile" />
+        <button className={styles.newPearl}>New Pearl</button>
       </nav>
     </>
   );
--- client/src/components/NavBar/NavButtons.js
+++ client/src/components/NavBar/NavButtons.js
@@ -1,31 +1,11 @@
-import home from "./icon/home.png";
-import news from "./icon/news.png";
-import chats from "./icon/chat.png";
-import profile from "./icon/profile.png";
 import { NavLink } from "react-router-dom";
 import styles from "./NavBar.module.css";
 const NavButtons = (props) => {
   return (
     <>
-      <NavLink to="/" className={styles.navBarLink}>
-        <button className={styles.NavBtn}>
-          <img src={home} alt="homeIcon" /> Home
-        </button>
-      </NavLink>
-      <NavLink to="/news" className={styles.navBarLink}>
-        <button className={styles.NavBtn}>
-          <img src={news} alt="newsIcon" /> News
-        </button>
-      </NavLink>
-      <NavLink to="/chats" className={styles.navBarLink}>
-        <button className={styles.NavBtn}>
-          <img src={chats} alt="chatsIcon" /> Chats
-        </button>
-      </NavLink>
-      <NavLink to="/profile" className={styles.navBarLink}>
-        <button className={styles.NavBtn}>
-          <img src={profile} alt="profilIcon" /> Profile
-        </button>
+      <NavLink to={props.path} className={styles.navBarLink}>
+        <img src={props.iconSource} alt="homeIcon" />
+        <span> {props.page} </span>
       </NavLink>
     </>
   );
