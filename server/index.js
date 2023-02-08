0f36baad4fc1a4dbc2596c08d1ccba3ffd886b20
Yasin Bhojani <yasinbhojani333@gmail.com>
2023-02-08 23:34:54 +0530

    cors added

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 const mail = require("./services/mail");
 
