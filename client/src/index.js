db7a2edb5873df6d93de93ea7e4b95267778392a
Yasin Bhojani <yasinbhojani333@gmail.com>
2023-03-12 19:00:29 +0530

    removed context

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import ModalContextProvider from "./context/ModalContextProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <ModalContextProvider>
-    <App />
-  </ModalContextProvider>
-);
+root.render(<App />);
